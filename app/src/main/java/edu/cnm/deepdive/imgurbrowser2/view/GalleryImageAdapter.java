1c4f97200233761c171dad287fd3f0c4fd95cddb
Christie Ryan <cryan11@cnm.edu>
2020-07-22 11:55:18 -0600

    Added image detail fragment, updated to list

--- app/src/main/java/edu/cnm/deepdive/imgurbrowser2/view/GalleryImageAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/imgurbrowser2/view/GalleryImageAdapter.java
@@ -12,12 +12,13 @@ import androidx.annotation.Nullable;
 import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.imgurbrowser2.R;
 import edu.cnm.deepdive.imgurbrowser2.model.Image;
+import java.util.List;
 
 public class GalleryImageAdapter extends ArrayAdapter<Image> {
 
   public GalleryImageAdapter(@NonNull Context context,
-      Image[] imageItemArray) {
-    super(context, 0, imageItemArray);
+      List<Image> imageList) {
+    super(context, 0, imageList);
   }
 
   @NonNull
--- app/src/main/java/edu/cnm/deepdive/imgurbrowser2/viewmodel/ListViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/imgurbrowser2/viewmodel/ListViewModel.java
@@ -3,35 +3,34 @@ package edu.cnm.deepdive.imgurbrowser2.viewmodel;
 import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.Lifecycle.Event;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.OnLifecycleEvent;
 import edu.cnm.deepdive.imgurbrowser2.BuildConfig;
 import edu.cnm.deepdive.imgurbrowser2.model.Gallery;
 import edu.cnm.deepdive.imgurbrowser2.service.ImgurService;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
+import java.util.List;
 
 public class ListViewModel extends AndroidViewModel implements LifecycleObserver {
 
-  private final MutableLiveData<Gallery.Search> searchResult;
+  private final MutableLiveData<List<Gallery>> galleries;
   private final MutableLiveData<Throwable> throwable;
   private final ImgurService imgurService;
   private final CompositeDisposable pending;
 
   public ListViewModel(@NonNull Application application) {
     super(application);
-    searchResult = new MutableLiveData<>();
-    throwable = new MutableLiveData<>();
+    galleries = new MutableLiveData<List<Gallery>>();
+    throwable = new MutableLiveData<Throwable>();
     imgurService = ImgurService.getInstance();
     pending = new CompositeDisposable();
     loadData();
   }
 
-  public LiveData<Gallery.Search> getSearchResult() {
-    return searchResult;
+  public LiveData<List<Gallery>> getGalleries() {
+    return galleries;
   }
 
   public LiveData<Throwable> getThrowable() {
@@ -42,9 +41,16 @@ public class ListViewModel extends AndroidViewModel implements LifecycleObserver
     pending.add(
         imgurService.getSearchResult(BuildConfig.CLIENT_ID, "cars")
             .subscribeOn(Schedulers.io())
+            .map((result) -> {
+              List<Gallery> galleries = result.getData();
+              galleries.removeIf((gallery) ->
+                  gallery.getImages() == null ||
+                      gallery.getImages().isEmpty());
+              return galleries;
+                })
             .subscribe(
-                searchResult -> {
-                  this.searchResult.postValue(searchResult);
+                value -> {
+                  this.galleries.postValue(value);
                 },
                 throwable -> this.throwable.postValue(throwable.getCause())
             )
